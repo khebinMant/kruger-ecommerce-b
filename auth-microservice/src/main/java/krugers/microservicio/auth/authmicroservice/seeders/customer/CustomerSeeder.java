10bf45d735b7cebaec945c743530ab4c3b68880e
khabLexander <mantillagka@hotmail.com>
2023-01-26 11:31:58 -0500

    Matriz

--- auth-microservice/src/main/java/krugers/microservicio/auth/authmicroservice/seeders/customer/CustomerSeeder.java
+++ auth-microservice/src/main/java/krugers/microservicio/auth/authmicroservice/seeders/customer/CustomerSeeder.java
@@ -23,15 +23,15 @@ public class CustomerSeeder {
 
         List<Address> addressesKev =  new ArrayList<>();
         addressesKev.add(new Address(1L,"Pichincha","Quito","Av Hernan Gmoiner","Calle 14",true,1L,"CREATED",new Date(), new Date()));
-        addressesKev.add(new Address(2L,"Pichincha","Quito","Es dirección del trabajo","Calle 14",true,1L,"CREATED",new Date(), new Date()));
+        addressesKev.add(new Address(2L,"Pichincha","Quito","Es dirección del trabajo","Calle 14",false,1L,"CREATED",new Date(), new Date()));
 
         List<Address> addressesKen =  new ArrayList<>();
         addressesKen.add(new Address(3L,"Guayas","Guayaquil","Av casa de kenan","Calle 14",true,2L,"CREATED",new Date(), new Date()));
-        addressesKen.add(new Address(4L,"Guayas","Guayaquil","Es dirección del trabajo","Calle 14",true,2L,"CREATED",new Date(), new Date()));
+        addressesKen.add(new Address(4L,"Guayas","Guayaquil","Es dirección del trabajo","Calle 14",false,2L,"CREATED",new Date(), new Date()));
 
         List<Address> addressesJos =  new ArrayList<>();
         addressesJos.add(new Address(5L,"Tungurahua","Ambato","Av casa de Jose Andrés","Calle 14",true,3L,"CREATED",new Date(), new Date()));
-        addressesJos.add(new Address(6L,"Tungurahua","Ambato","Es dirección del trabajo","Calle 14",true,3L,"CREATED",new Date(), new Date()));
+        addressesJos.add(new Address(6L,"Tungurahua","Ambato","Es dirección del trabajo","Calle 14",false,3L,"CREATED",new Date(), new Date()));
 
 
         userServiceImpl.save(new User(1L,"kevin@customer.com","12345678","Kevin","Mantilla","0986261197",new Date(),new Date(), "https://avatars.githubusercontent.com/u/33032880?v=4",1L,addressesKev, true, Role.CUSTOMER));
